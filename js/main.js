ff97b20adec4649fea93be531d16f5f9c6bd1073
Weronika Sosnowska <weronikasosnowska97@gmail.com>
2019-12-11 13:02:32 +0100

    Add files - first version

---
+++ js/main.js
@@ -0,0 +1,36 @@
+function scroll(e) {
+  var href = $(this).attr('href');
+  e.preventDefault();
+  $('html, body').animate({
+    scrollTop: $(href).offset().top
+  }, 800);
+  location.hash = href;
+};
+$('a[href^="#"]').click(scroll);
+
+$(function(){
+  var menu = $('#menu'),
+  pos = menu.offset();
+  $(window).scroll(function(){
+    if($(this).scrollTop() > pos.top+menu.height() && menu.hasClass('header')){
+      menu.fadeOut('fast', function(){
+        $(this).removeClass('header').addClass('fixed').fadeIn('fast');
+      });
+    } else if($(this).scrollTop() <= pos.top && menu.hasClass('fixed')){
+      menu.fadeOut('fast', function(){
+        $(this).removeClass('fixed').addClass('header').fadeIn('fast');
+      });
+    }
+  });
+});
+
+window.sr = ScrollReveal();
+sr.reveal('.icon', {
+  reset: true,
+  delay: 300,
+  distance: 0
+});
+sr.reveal('h1, h2, p, img', {
+  reset: true,
+  delay: 300
+});
